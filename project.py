996b27ef44e3fcc3527f328279e02164be706b8c
VolpeAzzurro <terrazoon@gmail.com>
2017-05-28 19:45:29 +0000

    feat: Add Edit and Delete Menu Item
    added methods to edit and delete menu items
    along with templates for them

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
 
@@ -12,21 +12,62 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)    
+    
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/edit',
+           methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, MenuID):
+    editedItem = session.query(MenuItem).filter_by(id=MenuID).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, MenuID=MenuID, item=editedItem)
+
+    
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/delete',
+           methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, MenuID):
+    print "hit delete menu item for restaurant id=%s menuID=%s" % (restaurant_id, MenuID)
+    item = session.query(MenuItem).filter_by(id=MenuID).one()
+    print "did we find the item? %s" % item.name
+    if request.method == 'POST':
+        print "method is POST"
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template(
+            'deletemenuitem.html', item=item)
+    
+
+    
 @app.route('/restaurants/<int:restaurant_id>/')
-def HelloWorld(restaurant_id):
+def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     print "got restaurant %s" % restaurant.name
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
-    for i in items:
-        print "got an item %s" % i.name
-        output += i.name
-        output += '<br>'
-        output += i.price
-        output += '<br>'
-        output += i.description
-        output += '<br><br>'
-    return output
+    return render_template('menu.html', restaurant = restaurant, items = items)
+    
     
 if __name__ == '__main__':
     print "hit main"
